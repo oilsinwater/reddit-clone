9bd5d20b7d7db503c20b77515606efba9f63b77e
Phil D <pcdrummond@icloud.com>
2023-03-10 12:10:13 -0800

    Creates Layout and Navbar components.

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,22 @@
+// eslint
+import React from 'react';
+import { Flex, Image } from '@chakra-ui/react';
+
+const Navbar: React.FC = () => {
+  return (
+    <Flex bg="white" height="44px" padding="6px 12px">
+      <Flex align="center">
+        <Image src="/images/redditFace.svg" height="30px" />
+        <Image
+          src="/images/redditText.svg"
+          height="46px"
+          display={{ base: 'none', md: 'unset' }}
+        />
+      </Flex>
+    </Flex>
+    // <Directory />
+    // <SearchInput />
+    // <RightContent />
+  );
+};
+export default Navbar;
